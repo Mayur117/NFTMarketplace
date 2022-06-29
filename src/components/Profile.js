f47f03ec55247a715397ce92327f99d7a7428cab
Mayur117 <thummar.mayur@gmail.com>
2022-06-29 16:01:52 +0530

    Added Alchemy API getNFTs and getNFTsForCollection

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,24 @@
 import Navbar from "./Navbar";
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import MarketplaceJSON from "../Marketplace.json";
-import axios from "axios";
 import { useState } from "react";
 import NFTTile from "./NFTTile";
+import { Network, initializeAlchemy, getNftsForCollection } from '@alch/alchemy-sdk';
 
-export default function Profile () {
+
+export default function Profile() {
+    // Using HTTPS
+
     const [data, updateData] = useState([]);
     const [dataFetched, updateFetched] = useState(false);
     const [address, updateAddress] = useState("0x");
     const [totalPrice, updateTotalPrice] = useState("0");
-
+    const settings = {
+        apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,  // Replace with your Alchemy API Key.
+        network: Network.ETH_GOERLI,  // Replace with your network.
+        maxRetries: 10
+      };
+    
     async function getNFTData(tokenId) {
         const ethers = require("ethers");
         let sumPrice = 0;
@@ -18,38 +26,39 @@ export default function Profile () {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
         const addr = await signer.getAddress();
+        console.log("Signer address :", addr);
 
         //Pull the deployed contract instance
         let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer)
 
-        //create an NFT Token
-        let transaction = await contract.getMyNFTs()
+        const alchemy = initializeAlchemy(settings);
+        // Print total NFT count returned in the response:
+        const nftsForCollection = await getNftsForCollection(
+            alchemy,
+            MarketplaceJSON.address
+        );
 
-        /*
-        * Below function takes the metadata from tokenURI and the data returned by getMyNFTs() contract function
-        * and creates an object of information that is to be displayed
-        */
+        let itemvalue = [];
+        await Promise.all(nftsForCollection.nfts.map(async i => {
         
-        const items = await Promise.all(transaction.map(async i => {
-            const tokenURI = await contract.tokenURI(i.tokenId);
-            let meta = await axios.get(tokenURI);
-            meta = meta.data;
-
-            let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
-            let item = {
-                price,
-                tokenId: i.tokenId.toNumber(),
-                seller: i.seller,
-                owner: i.owner,
-                image: meta.image,
-                name: meta.name,
-                description: meta.description,
+            //Alchemy API to get ListedToken details
+            let transaction = await contract.getListedTokenForId(i.tokenId);
+            if(addr === transaction.seller) {
+                let price = i.rawMetadata.price;
+                let item = {
+                    tokenId: i.tokenId,
+                    owner: MarketplaceJSON.address,
+                    image: i.rawMetadata.image,
+                    name: i.rawMetadata.name,
+                    description: i.description,
+                }
+                sumPrice += Number(price);
+                itemvalue.push(item);
             }
-            sumPrice += Number(price);
-            return item;
+
         }))
 
-        updateData(items);
+        updateData(itemvalue);
         updateFetched(true);
         updateAddress(addr);
         updateTotalPrice(sumPrice.toPrecision(3));
@@ -57,20 +66,20 @@ export default function Profile () {
 
     const params = useParams();
     const tokenId = params.tokenId;
-    if(!dataFetched)
+    if (!dataFetched)
         getNFTData(tokenId);
 
     return (
         <div className="profileClass" style={{"min-height":"100vh"}}>
             <Navbar></Navbar>
             <div className="profileClass">
-            <div className="flex text-center flex-col mt-11 md:text-2xl text-white">
-                <div className="mb-5">
-                    <h2 className="font-bold">Wallet Address</h2>  
-                    {address}
+                <div className="flex text-center flex-col mt-11 md:text-2xl text-white">
+                    <div className="mb-5">
+                        <h2 className="font-bold">Wallet Address</h2>
+                        {address}
+                    </div>
                 </div>
-            </div>
-            <div className="flex flex-row text-center justify-center mt-10 md:text-2xl text-white">
+                <div className="flex flex-row text-center justify-center mt-10 md:text-2xl text-white">
                     <div>
                         <h2 className="font-bold">No. of NFTs</h2>
                         {data.length}
@@ -79,18 +88,18 @@ export default function Profile () {
                         <h2 className="font-bold">Total Value</h2>
                         {totalPrice} ETH
                     </div>
-            </div>
-            <div className="flex flex-col text-center items-center mt-11 text-white">
-                <h2 className="font-bold">Your NFTs</h2>
-                <div className="flex justify-center flex-wrap max-w-screen-xl">
-                    {data.map((value, index) => {
-                    return <NFTTile data={value} key={index}></NFTTile>;
-                    })}
                 </div>
-                <div className="mt-10 text-xl">
-                    {data.length == 0 ? "Oops, No NFT data to display (Are you logged in?)":""}
+                <div className="flex flex-col text-center items-center mt-11 text-white">
+                    <h2 className="font-bold">Your NFTs</h2>
+                    <div className="flex justify-center flex-wrap max-w-screen-xl">
+                        {data.map((value, index) => {
+                            return <NFTTile data={value} key={index}></NFTTile>;
+                        })}
+                    </div>
+                    <div className="mt-10 text-xl">
+                        {data.length === 0 ? "Oops, No NFT data to display (Are you logged in?)" : ""}
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     )
